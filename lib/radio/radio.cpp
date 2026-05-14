7e82efb7ff664c5a98d16b44d66746a3fd12dceb
Aranykacsa <zalanvadasz@gmail.com>
2026-05-14 21:21:30 +0200

    mod: radio settings

--- lib/radio/radio.cpp
+++ lib/radio/radio.cpp
@@ -1,6 +1,12 @@
 #include "radio.hpp"
 
-uint8_t _init(void) {
+uint8_t _init(char* settings[SETTINGS_LENGTH]) {
+	Serial1.begin(115200);
+	for (uint8_t i = 0; i < SETTINGS_LENGTH; i++) {
+	    Serial1.print("radio set");
+	    Serial1.print(settings[i]);
+	    Serial1.print("\r\n");
+	}
     return 0;
 }
 
@@ -11,8 +17,11 @@ uint8_t _send(void) {
 
 radio_t radio = {
     .settings = {
-        "Hello World",
-        "General Kenobi",
+        "mod lora",
+	    "freq 868100000",
+	    "sf sf7",
+	    "pa off",
+	    "pwr 12"
     },
     .init = _init,
 	.send = _send,
--- lib/radio/radio.hpp
+++ lib/radio/radio.hpp
@@ -1,9 +1,16 @@
+#ifndef RADIO_HPP
+#define RADIO_HPP
+
 #include <Arduino.h>
 
+#define SETTINGS_LENGTH 5
+
 typedef struct radio_t {
-    const char* settings[2];
-    uint8_t (*init)(void);
+    const char* settings[SETTINGS_LENGTH];
+    uint8_t (*init)(char* settings[SETTINGS_LENGTH]);
 	uint8_t (*send)(void);
 } radio_t;
 
 extern radio_t radio;
+
+#endif /* RADIO_HPP */
